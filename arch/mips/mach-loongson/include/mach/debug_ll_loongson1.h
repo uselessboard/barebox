52650c526578b8103cf6d0da86e4e3460428986c
Du Huanpeng <u74147@gmail.com>
2020-09-11 16:12:01 +0800

    MIPS: loongson1: enable to run from spi flash
    initial pll, ddr2 controller, calculate perscale divisor for uart.
    
    Signed-off-by: Du Huanpeng <u74147@gmail.com>

---
+++ arch/mips/mach-loongson/include/mach/debug_ll_loongson1.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2020 Du Huanpeng <u74147@gmail.com>
+ */
+
+#ifndef __INCLUDE_DEBUG_LL_LOONGSON1_H__
+#define __INCLUDE_DEBUG_LL_LOONGSON1_H__
+
+#include <mach/loongson1.h>
+
+#define DEBUG_LL_UART_ADDR	0xBFE48000
+#define DEBUG_LL_UART_SHIFT	0
+#define DEBUG_LL_UART_DIVISOR	0
+
+#endif /* __INCLUDE_DEBUG_LL_LOONGSON1_H__ */
